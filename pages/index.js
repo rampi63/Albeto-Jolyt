9f23a15dd90560819c49e522181c9e017e16fb99
rampi63 <rampi63@gmail.com>
2023-02-12 14:14:45 -0600

    Trabajando mas secciones del cv

--- pages/index.js
+++ pages/index.js
@@ -25,42 +25,102 @@ export default function Home() {
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
       </Head>
       <div className={FontStyle(isDark)}>
-        <div className="banner">
-          <Header isDark={isDark} setIsDark={setIsDark}/>
-          <div className="flex justify-content-center align-items-center pt-8">
-            <div className="w-6 flex justify-content-end pr-8">
-              <img src="Saly-13.png"/>
-            </div>
-            <div className="flex flex-column gap-5 w-6">
-              <div className="font-25 font-bold flex gap-2">
-                <div>
-                  Hola! mi nombre es
-                </div>
-                <div style={{color:"#00BFFF !important"}}>
-                  Alberto Jolyt Cárdenas
-                </div> 
+        <div className="bgAll">
+          <div className="banner">
+            <Header isDark={isDark} setIsDark={setIsDark}/>
+            <div className="flex justify-content-center align-items-center pt-8 pb-4">
+              <div className="w-6 flex justify-content-end pr-8">
+                <img src="Saly-13.png"/>
               </div>
-              <h2 className="m-0">
-                <span className="font-bold" style={{color: "#00CAB1 !important"}}>
-                  Web developer
-                </span>
-                <br/>
-                <span className="font-light">
-                  con habilidades en:
-                </span>
-                <br/>
-                <span className="font-bold">
-                  React y Laravel
-                </span>
-              </h2>
-              <h6 className="m-0 font-light" style={{width:"60%"}}>
-                Destaco por mis habilidades de liderazgo en los proyectos y mi capacidad para conectar con los clientes para entender mejor sus necesidades.
-              </h6>
+              <div className="flex flex-column gap-5 w-6">
+                <div className="font-25 font-bold flex gap-2">
+                  <div>
+                    Hola! mi nombre es
+                  </div>
+                  <div style={{color:"#00BFFF !important"}}>
+                    Alberto Jolyt Cárdenas
+                  </div> 
+                </div>
+                <h2 className="m-0">
+                  <span className="font-bold" style={{color: "#00CAB1 !important"}}>
+                    Web developer
+                  </span>
+                  <br/>
+                  <span className="font-light">
+                    con habilidades en:
+                  </span>
+                  <br/>
+                  <span className="font-bold">
+                    React y Laravel
+                  </span>
+                </h2>
+                <h6 className="m-0 font-light" style={{width:"60%"}}>
+                  Destaco por mis habilidades de liderazgo en los proyectos y mi capacidad para conectar con los clientes para entender mejor sus necesidades.
+                </h6>
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          hola2
+          <div className="container flex flex-column">
+            <div>
+              <h1 className="first-section-title text-center m-0">
+                Soy capaz de crear interfaces y aplicaciones web de alta calidad
+              </h1>
+              <div className="mt-8 card-first-section">
+                <h6 className="text-center font-normal m-0">
+                  Mi ágil aprendizaje ayuda a adaptarme rápidamente a nuevas tecnologías y tendencias en la industria, lo que me permite estar siempre actualizado y ofrecer soluciones innovadoras. Mi estilo de presentación se enfoca en la comunicación efectiva y la atención al detalle. Me entusiasma aprender y especializarme en liderazgo de proyectos para contribuir al crecimiento y éxito de un equipo y una empresa.
+                </h6>
+              </div>
+            </div>
+            <div className="tech-section">
+              <h2 className="text-center font-bold m-0 pb-2">
+                Tecnologías
+              </h2>
+              <h6 className="text-center font-normal m-0">
+                A lo largo de mi trayectoria he trabajado con diversas herramientas que han aumentado mis habilidades.
+              </h6>
+              <div className="flex gap-6 pt-8 justify-content-center">
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+                <div style={{width:"73.77px",height:"73.77px", backgroundColor:"#3D1768",borderRadius:"50px"}}>
+
+                </div>
+              </div>
+              <div>
+
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- styles/fonts.css.js
+++ styles/fonts.css.js
@@ -4,11 +4,25 @@ export const FontStyle = (isDark=true) => {
 
     let fontFamily = "font-family: 'Montserrat', sans-serif;"
     let fontGeneralColor = isDark? "color: #FFFFFF;" : "color: #000000;"
+    let bgColor = isDark? "#1E1C29;" : "#fffff;"
+    let banner = isDark? "linear-gradient(to bottom, rgb(245 246 252 / 0%), rgb(30, 28, 41)), url(/_next/static/media/Background.62919cba.png);" : "linear-gradient(to bottom, rgb(245 246 252 / 0%), rgb(255 255 255)), url(/_next/static/media/Background.62919cba.png);"
 
     return css`
 
         ${fontFamily}
         ${fontGeneralColor}
+
+        transition: all 0.4s ease;
+
+        .bgAll{
+            background-color: ${bgColor}
+            transition: unset !important;
+            padding-bottom: 10rem;
+        }
+
+        h1{
+            font-size: 60px;
+        }
 
         h2{
             font-size: 55px;
@@ -29,6 +43,31 @@ export const FontStyle = (isDark=true) => {
 
         .font-25{
             font-size: 25px;
+        }
+
+        .banner{
+            background-image: ${banner}
+            background-size: cover;
+        }
+
+        .container{
+            padding: 0rem 10rem;
+            padding-top: 5rem;
+
+            .first-section-title{
+                padding: 0rem 10rem;
+            }
+
+            .card-first-section{
+                border-radius: 14px;
+                background-color: #262639;
+                padding: 1.5rem;
+                filter: drop-shadow(22px 17px 25px rgba(20, 78, 227, 0.1));
+            }
+
+            .tech-section{
+                padding-top: 15rem;
+            }
         }
     `
 }
